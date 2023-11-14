b1495f14618833654df335d0b56c05a40bc675ca
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-11-14 16:14:03 +0000

    Refactor wallet.ts to improve code structure and readability.

--- src/wallet.ts
+++ src/wallet.ts
@@ -39,50 +39,45 @@ export interface NetworkInfo {
     ensAddress?: Utils.Address;
 }
 
-export namespace Wallet {
-    export interface Base {}
-    export interface Connected extends Base {
-        connected: true;
-        method: ConnectMethod;
-        provider: ethers.BrowserProvider;
-        signer: ethers.JsonRpcSigner;
-        getChainId(): Promise<bigint>;
-        changeNetwork(network: NetworkInfo): Promise<boolean>;
-    }
+export interface Base {}
+export interface Connected extends Base {
+    connected: true;
+    method: ConnectMethod;
+    provider: ethers.BrowserProvider;
+    signer: ethers.JsonRpcSigner;
+    getChainId(): Promise<bigint>;
+    changeNetwork(network: NetworkInfo): Promise<boolean>;
+}
 
-    export interface Disconnected extends Base {
-        connected: false;
-    }
+export interface Disconnected extends Base {
+    connected: false;
+}
 
-    export async function connect(method: ConnectMethod, mode: "hard"): Promise<Wallet.Connected>;
-    export async function connect(method: ConnectMethod, mode: "soft"): Promise<Wallet.Connected | Wallet.Disconnected>;
-    export async function connect(
-        method: ConnectMethod,
-        mode: "soft" | "hard",
-    ): Promise<Wallet.Connected | Wallet.Disconnected> {
-        const provider = new ethers.BrowserProvider(
-            typeof method.ethereum === "function" ? await method.ethereum() : method.ethereum,
-        );
-        const signer =
-            (await provider.listAccounts().then((accounts) => accounts[0])) ?? mode === "hard"
-                ? await provider.getSigner()
-                : null;
-        if (!signer) return { connected: false };
+export async function connect(method: ConnectMethod, mode: "hard"): Promise<Connected>;
+export async function connect(method: ConnectMethod, mode: "soft"): Promise<Connected | Disconnected>;
+export async function connect(method: ConnectMethod, mode: "soft" | "hard"): Promise<Connected | Disconnected> {
+    const provider = new ethers.BrowserProvider(
+        typeof method.ethereum === "function" ? await method.ethereum() : method.ethereum,
+    );
+    const signer =
+        (await provider.listAccounts().then((accounts) => accounts[0])) ?? mode === "hard"
+            ? await provider.getSigner()
+            : null;
+    if (!signer) return { connected: false };
 
-        return {
-            connected: true,
-            method,
-            provider,
-            signer,
-            async getChainId() {
-                return await provider.getNetwork().then((network) => network.chainId);
-            },
-            async changeNetwork(network) {
-                await provider
-                    .send("wallet_switchEthereumChain", [{ chainId: ethers.toBeHex(network.chainId) }])
-                    .catch(() => {});
-                return await provider.getNetwork().then((network) => network.chainId === network.chainId);
-            },
-        };
-    }
+    return {
+        connected: true,
+        method,
+        provider,
+        signer,
+        async getChainId() {
+            return await provider.getNetwork().then((network) => network.chainId);
+        },
+        async changeNetwork(network) {
+            await provider
+                .send("wallet_switchEthereumChain", [{ chainId: ethers.toBeHex(network.chainId) }])
+                .catch(() => {});
+            return await provider.getNetwork().then((network) => network.chainId === network.chainId);
+        },
+    };
 }
