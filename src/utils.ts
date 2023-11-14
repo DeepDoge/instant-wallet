5c1ee921b469594e91236c1607f5475473e43b08
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-11-14 16:08:58 +0000

    Update package.json, tsconfig.json, and wallet.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,28 +1,28 @@
 export namespace Color {
-  export interface RGBA {
-    r: number;
-    g: number;
-    b: number;
-    a: number;
-  }
+    export interface RGBA {
+        r: number;
+        g: number;
+        b: number;
+        a: number;
+    }
 
-  export interface HSLA {
-    h: number;
-    s: number;
-    l: number;
-    a: number;
-  }
+    export interface HSLA {
+        h: number;
+        s: number;
+        l: number;
+        a: number;
+    }
 
-  export type Hex = `#${string}`;
+    export type Hex = `#${string}`;
 }
 
 export type ColorType = Color.RGBA | Color.HSLA | Color.Hex;
 export class Color<T extends ColorType = ColorType> {
-  public readonly color: T;
+    public readonly color: T;
 
-  constructor(init: T) {
-    this.color = init;
-  }
+    constructor(init: T) {
+        this.color = init;
+    }
 }
 
 export type Hex = `0x${string}`;
--- src/wallet.ts
+++ src/wallet.ts
@@ -18,28 +18,68 @@ export * as Utils from "./utils";
 */
 
 export interface Eip1193Provider extends ethers.Eip1193Provider {
-  on(event: string, listener: (...args: any[]) => void): void;
-  off(event: string, listener: (...args: any[]) => void): void;
-  once(event: string, listener: (...args: any[]) => void): void;
+    on(event: string, listener: (...args: any[]) => void): void;
+    off(event: string, listener: (...args: any[]) => void): void;
+    once(event: string, listener: (...args: any[]) => void): void;
 }
 
-export interface ConnectMethod extends ConnectMethod.Init {
-  provider: ethers.BrowserProvider;
-}
-export namespace ConnectMethod {
-  export interface Init {
+export interface ConnectMethod {
     name: string;
     icon: SVGElement;
     ethereum: Eip1193Provider;
-  }
 }
 
 export interface NetworkInfo {
-  chainId: bigint;
-  name: string;
-  icon?: SVGElement;
-  color?: Utils.Color;
-  rpcUrl: Utils.HttpUrl;
-  blockExplorer?: Utils.HttpUrl;
-  ensAddress?: Utils.Address;
+    chainId: bigint;
+    name: string;
+    icon?: SVGElement;
+    color?: Utils.Color;
+    rpcUrl: Utils.HttpUrl;
+    blockExplorer?: Utils.HttpUrl;
+    ensAddress?: Utils.Address;
+}
+
+export namespace Wallet {
+    export interface Base {}
+    export interface Connected extends Base {
+        connected: true;
+        method: ConnectMethod;
+        provider: ethers.BrowserProvider;
+        signer: ethers.JsonRpcSigner;
+        getChainId(): Promise<bigint>;
+        changeNetwork(network: NetworkInfo): Promise<boolean>;
+    }
+
+    export interface Disconnected extends Base {
+        connected: false;
+    }
+
+    export async function connect(method: ConnectMethod, mode: "hard"): Promise<Wallet.Connected>;
+    export async function connect(method: ConnectMethod, mode: "soft"): Promise<Wallet.Connected | Wallet.Disconnected>;
+    export async function connect(
+        method: ConnectMethod,
+        mode: "soft" | "hard",
+    ): Promise<Wallet.Connected | Wallet.Disconnected> {
+        const provider = new ethers.BrowserProvider(method.ethereum);
+        const signer =
+            (await provider.listAccounts().then((accounts) => accounts[0])) ?? mode === "hard"
+                ? await provider.getSigner()
+                : null;
+        if (!signer) return { connected: false };
+        return {
+            connected: true,
+            method,
+            provider,
+            signer,
+            async getChainId() {
+                return await provider.getNetwork().then((network) => network.chainId);
+            },
+            async changeNetwork(network) {
+                await provider
+                    .send("wallet_switchEthereumChain", [{ chainId: ethers.toBeHex(network.chainId) }])
+                    .catch(() => {});
+                return await provider.getNetwork().then((network) => network.chainId === network.chainId);
+            },
+        };
+    }
 }
